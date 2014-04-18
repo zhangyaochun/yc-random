d8f3dcbe40c67ae5364aadccd7028998dfe6b5a9
zhangyaochun <zhangyaochun@wandoujia.com>
2014-04-18 13:04:10 +0800

    update framework first time

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 'use strict';
 
+var random = module.exports = {};
+
+//natural
+random.natural = function (mim, max) {
+
+    if (arguments.length == 0) {
+        return 0;
+    }
+
+    if (max == null) {
+        max = min;
+        min = 0;
+    }
+
+    return min + Math.floor(Math.random() * (max - min) + 1);
+};
+
+//pick for array source
+random.pick = function (arr) {
+    //todo add isArray check
+    if (arguments.length > 0) {
+        return arr[random.natural(0,arr.length - 1)];
+    }
+};
+
+
+function rRequire(name) {
+    random[name] = require('./lib/' + name);
+};
+
+rRequire('util');
+rRequire('address');
+rRequire('data');
+rRequire('color');
+
+//image relay color
+rRequire('image');
+
+rRequire('word');
+
+//web relay word
+var web = rRequire('web');
+
+//version from package.json
+random.version = require('package.json').version;
+
+
+function rExpose(obj) {
+   for (var key in obj) {
+      grunt[key] = obj[key].bind(obj);
+   }
+}
+
+//TODO
+//how to expose all obj's function?
+//or not to?
+gExpose(web, 'url');
