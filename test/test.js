ccc921cc2ad9181f7f014f64541abebc1a89f2ba
zhangyaochun <zhangyaochun@wandoujia.com>
2015-03-10 17:02:53 +0800

    [command line] commit auto by @wd-deploy on 2015-03-10 05:02:53

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var random = require('../index');
 
+console.log(random.version);
+
 console.log(random.web.ip());
 console.log(random.web.domain());
 
 console.log(random.word.word());
+console.log(random.word.character());
+console.log(random.word.character('upper'));
 
 console.log(random.uuid());
